a91bd89dc42ae1e96a7ad66885cf008cbb147760
kenb9027 <kenb9027@gmail.com>
2021-03-17 09:42:45 +0100

    fix menu on mobile with click

--- public/app/js/menu.js
+++ public/app/js/menu.js
@@ -1,5 +1,7 @@
 const menu = document.getElementById('menu');
 const menuList = document.getElementById('menu-list');
+const dropDowns = document.querySelectorAll('.dropdown');
+const menuLinks = document.querySelectorAll('.menu-link');
 
 function moveBurger(x) {
     x.classList.toggle("change");
@@ -7,3 +9,11 @@ function moveBurger(x) {
     menuList.classList.toggle('menu-list-mobile');
 
   };
+dropDowns.forEach((dd) => {
+  const ddc = dd.querySelector(".dropdown-content");
+
+  dd.addEventListener("click", (e) => {
+    e.preventDefault() ;
+    ddc.classList.toggle("clickOnDropdown");
+  })
+})
