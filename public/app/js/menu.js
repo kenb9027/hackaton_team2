616ea1dd0e824c484c0e8b4c9626ad5048afa4dd
Antoninglz33 <antonin33gonzalez@gmail.com>
2021-03-17 13:59:09 +0100

    commit avant push

---
+++ public/app/js/menu.js
@@ -0,0 +1,18 @@
+const menu = document.getElementById("menu");
+const menuList = document.getElementById("menu-list");
+const dropDowns = document.querySelectorAll(".dropdown");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function moveBurger(x) {
+  x.classList.toggle("change");
+  menu.classList.toggle("menu-mobile");
+  menuList.classList.toggle("menu-list-mobile");
+}
+dropDowns.forEach((dd) => {
+  const ddc = dd.querySelector(".dropdown-content");
+
+  dd.addEventListener("click", (e) => {
+    e.preventDefault();
+    ddc.classList.toggle("clickOnDropdown");
+  });
+});
